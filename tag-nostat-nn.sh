93522b20778c9f23e01681c9090733456566b595
Anders NÃ¸klestad <anders.noklestad@iln.uio.no>
2011-06-02 13:57:47 +0200

    Fix script for nynorsk tagging

--- tag-nostat-nn.sh
+++ tag-nostat-nn.sh
@@ -6,6 +6,6 @@ then
   exit $E_BADARGS
 fi
 
-bin/mtag -wxml < $1 | vislcg3 -C latin1 --codepage-input \
-  utf-8 -g cg/bm_morf.cg --codepage-output utf-8 | \
+bin/mtag -wxml -nn < $1 | vislcg3 -C latin1 --codepage-input \
+  utf-8 -g cg/nn_morf.cg --codepage-output utf-8 | \
   perl -ne 'print if /\S/'
