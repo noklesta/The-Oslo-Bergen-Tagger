d350c83abbadcd6adb7143f4331f965f066da8af
Bjarte Johansen <bjarte.johansen@gmail.com>
2015-05-06 11:23:25 +0200

    Make scripts callable outside of OBT directory

--- tag-nostat-nn.sh
+++ tag-nostat-nn.sh
@@ -6,6 +6,13 @@ then
   exit $E_BADARGS
 fi
 
-bin/mtag -wxml -nn < $1 | vislcg3 -C latin1 --codepage-input \
-  utf-8 -g cg/nn_morf.cg --codepage-output utf-8 --no-pass-origin -e | \
-  perl -ne 'print if /\S/'
+OBT=$(dirname $0)
+
+"$OBT"/bin/mtag -wxml -nn < $1			\
+    | vislcg3 --codepage-all latin1		\
+	      --codepage-input utf-8		\
+	      --grammar "$OBT"/cg/nn_morf.cg	\
+	      --codepage-output utf-8		\
+	      --no-pass-origin			\
+	      --show-end-tags			\
+   | perl -ne 'print if /\S/'
