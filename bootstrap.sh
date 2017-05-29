a71b964849868f7834a6eb4a32b8bc13649e27ee
Mats Byrkjeland <matsbyr@gmail.com>
2017-05-29 15:47:18 +0200

    Fix check for Linux in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ if [ ! -f bin/mtag ]; then
     BASE_URL='http://www.tekstlab.uio.no/mtag'
     if [[ "$OSTYPE" == "darwin"* ]]; then
         wget "${BASE_URL}/osx64/mtag-osx64" -O bin/mtag
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
         wget "${BASE_URL}/linux64/mtag-linux-1-11" -O bin/mtag
     else
         echo "Your platform $OSTYPE is not supported."
